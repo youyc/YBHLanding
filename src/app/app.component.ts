ca2bc00cb85b4dd2db8ddd70b8f38ebe78be7aa2
YC <youyongchean@gmail.com>
2025-02-23 21:40:44 +0800

    #develop: finalize section1 layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppTopToolbarComponent } from '@config/component/app-top-toolbar/app-top-toolbar.component';
 
@@ -7,6 +7,7 @@ import { AppTopToolbarComponent } from '@config/component/app-top-toolbar/app-to
   imports: [RouterOutlet, AppTopToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
   title = 'LandingPage';
